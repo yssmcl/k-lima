c844ba214c4a0a3c66d981bf443d9286859611b9
yssmcl <yuri_1704@hotmail.com>
2017-07-25 20:48:59 -0300

    Back-end da geração de relatório da página tabela

--- Code/K-lima/src/java/unioeste/geral/relatorio/Relatorio.java
+++ Code/K-lima/src/java/unioeste/geral/relatorio/Relatorio.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -13,6 +14,8 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 import unioeste.geral.bo.Aluno;
 import unioeste.geral.bo.Curso;
 import unioeste.geral.manager.AlunoManager;
@@ -24,8 +27,8 @@ public class Relatorio {
 			String diretorioAtual = System.getProperty("user.dir");
 			diretorioAtual = diretorioAtual.split("k-lima")[0];
 			new File(diretorioAtual + "k-lima/lib/apache-tomcat-8.5.15/temp").mkdir(); // cria diretório temp pra não dar exceção de fontes no jasper
-			String arquivoTemplate = diretorioAtual + "k-lima/Code/K-lima/jasper/templates/relatorioabandono.jrxml";
-			String arquivoDestino = diretorioAtual + "k-lima/Code/K-lima/jasper/relatorios/relatorioabandono.pdf";
+			String arquivoTemplate = diretorioAtual + "k-lima/Code/K-lima/jasper/templates/relatorio-abandono.jrxml";
+			String arquivoDestino = diretorioAtual + "k-lima/Code/K-lima/jasper/relatorios/relatorio-abandono.pdf";
 			String caminhoLogoUnioeste = diretorioAtual + "k-lima/Code/K-lima/web/img/unioeste.jpg";
 			String caminhoLogoGoverno = diretorioAtual + "k-lima/Code/K-lima/web/img/governo.jpg";
 
@@ -66,14 +69,42 @@ public class Relatorio {
 			JasperReport jasperReport = JasperCompileManager.compileReport(arquivoTemplate);
 			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, new JREmptyDataSource());
 			JasperExportManager.exportReportToPdfFile(jasperPrint, arquivoDestino);
-//			JasperViewer.viewReport(jasperPrint);
+			JasperViewer.viewReport(jasperPrint);
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void gerarRelatorioGeral(List<Aluno> alunos) {
+		try {
+			String diretorioAtual = System.getProperty("user.dir");
+			diretorioAtual = diretorioAtual.split("k-lima")[0];
+			new File(diretorioAtual + "k-lima/lib/apache-tomcat-8.5.15/temp").mkdir();
+			String arquivoTemplate = diretorioAtual + "k-lima/Code/K-lima/jasper/templates/relatorio-geral.jrxml";
+			String arquivoDestino = diretorioAtual + "k-lima/Code/K-lima/jasper/relatorios/relatorio-geral.pdf";
+			String caminhoLogoUnioeste = diretorioAtual + "k-lima/Code/K-lima/web/img/unioeste.jpg";
+			String caminhoLogoGoverno = diretorioAtual + "k-lima/Code/K-lima/web/img/governo.jpg";
+			//String Report ="/home/cristopher/NetBeansProjects/Geradorderelatório/relatorio-geral.jrxml";
+
+			JasperReport jr = JasperCompileManager.compileReport(arquivoTemplate);
+
+			Map<String, Object> parametros = new HashMap<>();
+
+			JRDataSource arquivo = new JRBeanCollectionDataSource(alunos);
+
+			JasperPrint jp = JasperFillManager.fillReport(jr, parametros, arquivo);
+			JasperExportManager.exportReportToPdfFile(jp, arquivoDestino);
+			JasperViewer.viewReport(jp);
 		} catch (JRException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
-		new Relatorio().gerarRelatorioEvasaoPorCurso(null);
+		Multimap<String, Object> condicaoAND = HashMultimap.create();
+		condicaoAND.put("situacaoAtual", "Cancelado");
+		List<Aluno> alunosCancelados = new AlunoManager().recuperarAlunosPorAtributosMultimap(condicaoAND, null);
+		new Relatorio().gerarRelatorioGeral(alunosCancelados);
 	}
 
 }
