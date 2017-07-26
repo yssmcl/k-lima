45fdecd87983162c540192d32979a7b92ff98550
Cristophervidalmachado <cristophervidal3@gmail.com>
2017-07-26 15:39:10 -0300

    "Alteração do relatório"

--- Code/K-lima/src/java/Main.java
+++ Code/K-lima/src/java/Main.java
@@ -1,6 +1,9 @@
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import unioeste.geral.bo.Curso;
 import unioeste.geral.manager.AlunoManager;
 import unioeste.geral.manager.CursoManager;
@@ -8,26 +11,26 @@ import unioeste.geral.manager.CursoManager;
 public class Main {
 
 	public static void main(String[] args) {
-		Multimap<String, Object> condicaoAND = HashMultimap.create();
-		Curso curso = new CursoManager().recuperarCursosPorAtributo("nome", "Ciência da Computação").get(0);
-		condicaoAND.put("curso", curso);
-
-		Multimap<String, Object> condicaoOR = HashMultimap.create();
-		condicaoOR.put("situacaoAtual", "Cancelado Por Abandono");
-		condicaoOR.put("situacaoAtual", "Cancelado");
-		condicaoOR.put("situacaoAtual", "Transferido");
-		Long qtdAlunos = new AlunoManager().recuperarQtdAlunosPorAtributos(condicaoAND, condicaoOR);
-		System.out.println("=========================== " + qtdAlunos);
+//		Multimap<String, Object> condicaoAND = HashMultimap.create();
+//		Curso curso = new CursoManager().recuperarCursosPorAtributo("nome", "Ciência da Computação").get(0);
+//		condicaoAND.put("curso", curso);
+//
+//		Multimap<String, Object> condicaoOR = HashMultimap.create();
+//		condicaoOR.put("situacaoAtual", "Cancelado Por Abandono");
+//		condicaoOR.put("situacaoAtual", "Cancelado");
+//		condicaoOR.put("situacaoAtual", "Transferido");
+//		Long qtdAlunos = new AlunoManager().recuperarQtdAlunosPorAtributos(condicaoAND, condicaoOR);
+//		System.out.println("=========================== " + qtdAlunos);
 
 		/* Teste de conexão */
-//		try {
-//			Connection connection;
-//			Class.forName("org.mariadb.jdbc.Driver");
-//			connection = DriverManager.getConnection("jdbc:mysql://localhost/db_klima", "root", "");
-//			System.out.println(connection.isValid(0));
-//		} catch (ClassNotFoundException | SQLException ex) {
-//			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//		}
+		try {
+			Connection connection;
+			Class.forName("org.mariadb.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/db_klima", "root", "");
+			System.out.println(connection.isValid(0));
+		} catch (ClassNotFoundException | SQLException ex) {
+			System.out.println("Deu merda");
+		}
 	}
 
 }
--- Code/K-lima/src/java/unioeste/geral/relatorio/Relatorio.java
+++ Code/K-lima/src/java/unioeste/geral/relatorio/Relatorio.java
@@ -80,8 +80,8 @@ public class Relatorio {
 			String diretorioAtual = System.getProperty("user.dir");
 			diretorioAtual = diretorioAtual.split("k-lima")[0];
 			new File(diretorioAtual + "k-lima/lib/apache-tomcat-8.5.15/temp").mkdir();
-			String arquivoTemplate = diretorioAtual + "k-lima/Code/K-lima/jasper/templates/relatorio-geral.jrxml";
-			String arquivoDestino = diretorioAtual + "k-lima/Code/K-lima/jasper/relatorios/relatorio-geral.pdf";
+			String arquivoTemplate = "/home/cristopher/Área de Trabalho/tudo/k-lima/k-lima/Code/K-lima/jasper/templates/relatorio-geral.jrxml"; 
+			//String arquivoDestino = diretorioAtual + "k-lima/Code/K-lima/jasper/relatorios/relatorio-geral.pdf";
 			String caminhoLogoUnioeste = diretorioAtual + "k-lima/Code/K-lima/web/img/unioeste.jpg";
 			String caminhoLogoGoverno = diretorioAtual + "k-lima/Code/K-lima/web/img/governo.jpg";
 			//String Report ="/home/cristopher/NetBeansProjects/Geradorderelatório/relatorio-geral.jrxml";
@@ -93,7 +93,7 @@ public class Relatorio {
 			JRDataSource arquivo = new JRBeanCollectionDataSource(alunos);
 
 			JasperPrint jp = JasperFillManager.fillReport(jr, parametros, arquivo);
-			JasperExportManager.exportReportToPdfFile(jp, arquivoDestino);
+                       // JasperExportManager.exportReportToPdfFile(jp, arquivoDestino);
 			JasperViewer.viewReport(jp);
 		} catch (JRException e) {
 			e.printStackTrace();
