ccf222913f013a4efc1368c0df8aadd386a6ef5e
Nardi <felipebbnn@gmail.com>
2017-07-31 15:02:23 -0300

    Arrumada a classe Disciplina.

--- Code/K-lima/src/java/unioeste/geral/bo/Disciplina.java
+++ Code/K-lima/src/java/unioeste/geral/bo/Disciplina.java
@@ -15,65 +15,72 @@ public class Disciplina {
 	private String nome;
 
 	private int serie;
+        
+                  private String codigo;
 
 	// dataInicio == null significa que o curso sempre existiu
-	private int anoInicio;
+	private Integer anoInicio;
 
 	// dataFim == null significa que o curso ainda existe
-	private int anoFim;
+	private Integer anoFim;
 
 	@ManyToOne
 	private Curso curso;
 
-	public Disciplina() {
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public int getSerie() {
+        return serie;
+    }
 
-	public int getSerie() {
-		return serie;
-	}
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
 
-	public void setSerie(int serie) {
-		this.serie = serie;
-	}
+    public String getCodigo() {
+        return codigo;
+    }
 
-	public int getAnoInicio() {
-		return anoInicio;
-	}
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-	public void setAnoInicio(int anoInicio) {
-		this.anoInicio = anoInicio;
-	}
+    public Integer getAnoInicio() {
+        return anoInicio;
+    }
 
-	public int getAnoFim() {
-		return anoFim;
-	}
+    public void setAnoInicio(Integer anoInicio) {
+        this.anoInicio = anoInicio;
+    }
 
-	public void setAnoFim(int anoFim) {
-		this.anoFim = anoFim;
-	}
+    public Integer getAnoFim() {
+        return anoFim;
+    }
 
-	public Curso getCurso() {
-		return curso;
-	}
+    public void setAnoFim(Integer anoFim) {
+        this.anoFim = anoFim;
+    }
 
-	public void setCurso(Curso curso) {
-		this.curso = curso;
-	}
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
 
 }
